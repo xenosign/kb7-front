2e97bbfc8c28c31277572203427faed83a3b69e2
kdtTetz <xenosign@naver.com>
2026-03-09 22:01:16 +0900

    feat: 수업용 left-pad 코드 추가

---
+++ 03_JS/ex1_leftpad-og.js
@@ -0,0 +1,18 @@
+function leftpad(str, len, ch) {
+  str = String(str);
+
+  var i = -1;
+
+  if (!ch && ch !== 0) ch = ' ';
+
+  len = len - str.length;
+
+  while (++i < len) {
+    str = ch + str;
+  }
+
+  return str;
+}
+
+const str = '123';
+console.log(leftpad(str, 5));
---
+++ 03_JS/ex1_leftpad-up.js
@@ -0,0 +1,17 @@
+function leftpad(str, len, ch) {
+  str = String(str);
+  len = len - str.length;
+  if (len < 1) return str;
+
+  if (!ch && ch !== 0) ch = ' ';
+  let i = -1;
+
+  while (++i < len) {
+    str = ch + str;
+  }
+
+  return str;
+}
+
+const str = '123';
+console.log(leftpad(str, 5));
