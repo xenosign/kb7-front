b7938e5cefc07960bbea2886c63b8bf81893978c
Tetz <xenosign@naver.com>
2026-03-11 11:52:39 +0900

    260311 수업 코드 완료

---
+++ 03_JS/17-ex_you-know.js
@@ -0,0 +1,14 @@
+// youKnow() 함수를 작성해 주세요
+function youKnow(name, cb) {
+  console.log("You know ~");
+  cb(name);
+}
+
+function sayMyName(name) {
+  console.log(`엄.... 제 이름은 ${name} 입니다`);
+}
+
+youKnow("이효석", sayMyName);
+youKnow("이효석", function (name) {
+  console.log(`저는 ${name} 이고, 취미는 자전거를 탑니다`);
+});
